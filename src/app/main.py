d10c16d17f430174894933219675803a0054a753
KennethMunyuza-hope <kenneth.munyuza@hope.edu>
2023-05-09 20:42:00 -0400

    partially functional app

--- src/app/main.py
+++ src/app/main.py
@@ -1,43 +1,131 @@
+from fast_autocomplete import autocomplete_factory
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.models import Sequential
+import tensorflow as tf
+import cv2
+import mediapipe as mp
 import os
 import base64
+import numpy as np
 from flask import Flask, request, jsonify, render_template, send_from_directory
 
 app = Flask(__name__)
 
-VIDEO_FOLDER = 'videos'
-app.config['VIDEO_FOLDER'] = VIDEO_FOLDER
-os.makedirs(VIDEO_FOLDER, exist_ok=True)
 
+content_files = {
+    'words': {
+        'filepath': 'word_dict.json',
+        'compress': True  # means compress the graph data in memory
+    }
+}
+alphabet = ['1', '2', '3', 'space', 'del', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+            'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+labels_ = ['1', '2', '3', 'space', 'del', 'A', 'B', 'C']
+# NUM_CLASSES = len(labels_)
+# model = Sequential()
+# # Create a checkpoint object and restore the weights
+# checkpoint_path = "model_checkpoints/cp_numbers_1.ckpt"
+
+# model.add(LSTM(64, return_sequences=True,
+#           activation='relu', input_shape=(21, 3)))
+# model.add(LSTM(128, return_sequences=True, activation='relu'))
+# model.add(LSTM(64, return_sequences=False, activation='relu'))
+# model.add(Dense(64, activation='relu'))
+# model.add(Dense(32, activation='relu'))
+# model.add(Dense(NUM_CLASSES, activation='softmax'))
+
+# autocomplete = autocomplete_factory(content_files=content_files)
+# model.load_weights(checkpoint_path)
+model = tf.keras.models.load_model("model/")
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+def most_frequent(List):
+    return max(set(List), key=List.count)
+
+def run_inference(image):
+    with mp_hands.Hands(
+            model_complexity=0,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as hands:
+        
+        # decode numpy array into OpenCV BGR image
+        image = cv2.imdecode(image, flags=1)
+        results = hands.process(image)
+
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        landmarks = []
+        letter = ''
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    image,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+
+                # Get the bounding box of the hand
+                x_min, y_min, x_max, y_max = float('inf'), float(
+                    'inf'), float('-inf'), float('-inf')
+                for landmark in hand_landmarks.landmark:
+                    x, y = landmark.x, landmark.y
+                    x_min = min(x_min, x)
+                    y_min = min(y_min, y)
+                    x_max = max(x_max, x)
+                    y_max = max(y_max, y)
+
+                # Normalize the coordinates with respect to the bounding box of the hand
+                for landmark in hand_landmarks.landmark:
+                    x, y, z = landmark.x, landmark.y, landmark.z
+                    x_norm = (x - x_min) / (x_max - x_min)
+                    y_norm = (y - y_min) / (y_max - y_min)
+                    landmarks.append([x_norm, y_norm, z])
+
+            # Convert the landmarks to a feature vector
+            x_t = np.array(landmarks)
+
+            if x_t.flatten().shape[0] == 63:
+                res_ = model.predict(np.array([x_t]), verbose=0)
+
+                index_class = np.argmax(res_)
+
+                letter = alphabet[index_class]
+    return letter
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-
-@app.route('/recognize', methods=['POST'])
+@app.route('/api/recognize', methods=['POST'])
 def recognize():
     data = request.get_json()
-    video_data = base64.b64decode(data['video'])
-
-    video_path = os.path.join(app.config['VIDEO_FOLDER'], 'asl_video.webm')
-
-    with open(video_path, 'wb') as f:
-        f.write(video_data)
-
-    # Process the video with your ASL recognition model here
-    result = process_video(video_path)  # Replace this function with the actual ASL recognition model
-    return jsonify({'result': result})
-
-
-def process_video(video_path):
-    # Add your ASL recognition model code here
-    result = "ASL recognition result"  # Replace this with the actual result from your model
-    return result
+    img_data = base64.b64decode(data['image'])
+    img_array = np.frombuffer(img_data, dtype=np.uint8)
+    # print(f'here is img_array -> {img_array}')
+    # print(f'{img_array.size}')
+    # TODO: do something with image array
+    if img_array.size == 0:
+        sign = ''
+    else:
+        sign = run_inference(img_array)
+    # sign = 'hello world'  # return hello world for now
+    return jsonify({'sign': sign})
 
 # Disable caching for CSS files
 @app.route('/static/css/<path:path>')
 def send_css(path):
     return send_from_directory('static/css', path, cache_timeout=0)
+
+# Disable caching for JS files
+@app.route('/static/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path, cache_timeout=0)
 
 if __name__ == '__main__':
     app.run(port=5003, debug=True)
--- src/app/static/js/app.js
+++ src/app/static/js/app.js
@@ -6,123 +6,189 @@ const replayButton = document.getElementById('replayBtn');
 const approveButton = document.getElementById('approve');
 const timerElement = document.getElementById('timer');
 const resultElement = document.getElementById('result');
+const resultDiv = document.getElementById("result");
 
-let recordedChunks = [];
-let mediaRecorder;
+let stream;
+let interval;
+let recognizedSigns = [];
 
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-        video.srcObject = stream;
-    });
+startButton.addEventListener("click", async () => {
+    startButton.disabled = true;
+    stopButton.disabled = false;
 
-startButton.addEventListener('click', async () => {
-    timerElement.textContent = '3';
-    startButton.style.display = 'none';
-    await sleep(1000);
-    timerElement.textContent = '2';
-    await sleep(1000);
-    timerElement.textContent = '1';
-    await sleep(1000);
-    timerElement.textContent = '';
-
-    startRecording();
+    stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false});
+    video.srcObject = stream;
+    console.log("here")
+    interval = setInterval(captureAndSendFrame, 1000); // Send a frame every 1000ms (1 second)
+    // TODO for Kenneth, maybe send images every 100 ms then server will just wait until 1 second
+    // this will give server 10 images to work with.
 });
 
-stopButton.addEventListener('click', () => {
-    stopRecording();
+stopButton.addEventListener("click", () => {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+
+    clearInterval(interval);
+    video.srcObject.getTracks().forEach(track => track.stop());
 });
 
-replayButton.addEventListener('click', () => {
-    replay.play();
-});
+async function captureAndSendFrame() {
+    const canvas = document.createElement("canvas");
+    console.log(video.videoWidth, video.videoHeight)
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const context = canvas.getContext("2d");
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    const imageData = canvas.toDataURL("image/jpeg", 0.8).split(",")[1];
 
-approveButton.addEventListener('click', async () => {
-    replay.pause();
-    replay.currentTime = 0;
-    const blob = new Blob(recordedChunks, { type: 'video/webm' });
-
-    const videoData = await blobToArrayBuffer(blob);
-    const base64Data = arrayBufferToBase64(videoData);
-
-    const response = await fetch('/recognize', {
-        method: 'POST',
-        body: JSON.stringify({ video: base64Data }),
-        headers: { 'Content-Type': 'application/json' }
-    });
-
-
-    if (response.ok) {
-        const { result } = await response.json();
-        resultElement.textContent = result;
-    } else {
-        resultElement.textContent = 'Error recognizing ASL.';
-    }
-
-    resetUI();
-});
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function startRecording() {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-        .then(stream => {
-            mediaRecorder = new MediaRecorder(stream);
-            mediaRecorder.ondataavailable = (event) => {
-                if (event.data.size > 0) {
-                    recordedChunks.push(event.data);
-                }
-            };
-            mediaRecorder.start();
-            stopButton.style.display = 'inline-block';
+    try {
+        const response = await fetch("/api/recognize", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ image: imageData })
         });
-}
 
-function stopRecording() {
-    mediaRecorder.stop();
-    stopButton.style.display = 'none';
-    replayButton.style.display = 'inline-block';
-    approveButton.style.display = 'inline-block';
-    video.style.display = 'none';
-    replay.style.display = 'inline-block';
-
-    setTimeout(() => {
-        const blob = new Blob(recordedChunks, { type: 'video/webm' });
-        replay.src = URL.createObjectURL(blob);
-    }, 100);
-}
-
-function resetUI() {
-    video.style.display = 'inline-block';
-    replay.style.display = 'none';
-    startButton.style.display = 'inline-block';
-    replayButton.style.display = 'none';
-    approveButton.style.display = 'none';
-    recordedChunks = [];
-}
-
-function blobToArrayBuffer(blob) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = () => {
-            resolve(reader.result);
-        };
-        reader.onerror = () => {
-            reject(new Error('Error reading Blob as ArrayBuffer'));
-        };
-        reader.readAsArrayBuffer(blob);
-    });
-}
-
-function arrayBufferToBase64(buffer) {
-    const binary = [];
-    const bytes = new Uint8Array(buffer);
-    const len = bytes.byteLength;
-
-    for (let i = 0; i < len; i++) {
-        binary.push(String.fromCharCode(bytes[i]));
+        const result = await response.json();
+        recognizedSigns.push(result.sign);
+        console.log(result.sign)
+        // updateResultDisplay();
+        // resultDiv.innerHTML = `<p>${result.sign}</p>`;
+    } catch (error) {
+        console.error("Error sending frame to the server:", error);
     }
-
-    return btoa(binary.join(''));
 }
+
+function updateResultDisplay() {
+    /* 
+        * This function updates the current results that we have ... we'll modify to have a sentence and
+        * not bullet points ... 
+        */
+    resultDiv.innerHTML = "<ul>";
+    recognizedSigns.forEach(sign => {
+        resultDiv.innerHTML += `<li>${sign}</li>`;
+    });
+    resultDiv.innerHTML += "</ul>";
+    
+}
+
+// let recordedChunks = [];
+// let mediaRecorder;
+
+// navigator.mediaDevices.getUserMedia({ video: true })
+//     .then(stream => {
+//         video.srcObject = stream;
+//     });
+
+// startButton.addEventListener('click', async () => {
+//     timerElement.textContent = '3';
+//     startButton.style.display = 'none';
+//     await sleep(1000);
+//     timerElement.textContent = '2';
+//     await sleep(1000);
+//     timerElement.textContent = '1';
+//     await sleep(1000);
+//     timerElement.textContent = '';
+
+//     startRecording();
+// });
+
+// stopButton.addEventListener('click', () => {
+//     stopRecording();
+// });
+
+// replayButton.addEventListener('click', () => {
+//     replay.play();
+// });
+
+// approveButton.addEventListener('click', async () => {
+//     replay.pause();
+//     replay.currentTime = 0;
+//     const blob = new Blob(recordedChunks, { type: 'video/webm' });
+
+//     const videoData = await blobToArrayBuffer(blob);
+//     const base64Data = arrayBufferToBase64(videoData);
+
+//     const response = await fetch('/recognize', {
+//         method: 'POST',
+//         body: JSON.stringify({ video: base64Data }),
+//         headers: { 'Content-Type': 'application/json' }
+//     });
+
+
+//     if (response.ok) {
+//         const { result } = await response.json();
+//         resultElement.textContent = result;
+//     } else {
+//         resultElement.textContent = 'Error recognizing ASL.';
+//     }
+
+//     resetUI();
+// });
+
+// function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// function startRecording() {
+//     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+//         .then(stream => {
+//             mediaRecorder = new MediaRecorder(stream);
+//             mediaRecorder.ondataavailable = (event) => {
+//                 if (event.data.size > 0) {
+//                     recordedChunks.push(event.data);
+//                 }
+//             };
+//             mediaRecorder.start();
+//             stopButton.style.display = 'inline-block';
+//         });
+// }
+
+// function stopRecording() {
+//     mediaRecorder.stop();
+//     stopButton.style.display = 'none';
+//     replayButton.style.display = 'inline-block';
+//     approveButton.style.display = 'inline-block';
+//     video.style.display = 'none';
+//     replay.style.display = 'inline-block';
+
+//     setTimeout(() => {
+//         const blob = new Blob(recordedChunks, { type: 'video/webm' });
+//         replay.src = URL.createObjectURL(blob);
+//     }, 100);
+// }
+
+// function resetUI() {
+//     video.style.display = 'inline-block';
+//     replay.style.display = 'none';
+//     startButton.style.display = 'inline-block';
+//     replayButton.style.display = 'none';
+//     approveButton.style.display = 'none';
+//     recordedChunks = [];
+// }
+
+// function blobToArrayBuffer(blob) {
+//     return new Promise((resolve, reject) => {
+//         const reader = new FileReader();
+//         reader.onload = () => {
+//             resolve(reader.result);
+//         };
+//         reader.onerror = () => {
+//             reject(new Error('Error reading Blob as ArrayBuffer'));
+//         };
+//         reader.readAsArrayBuffer(blob);
+//     });
+// }
+
+// function arrayBufferToBase64(buffer) {
+//     const binary = [];
+//     const bytes = new Uint8Array(buffer);
+//     const len = bytes.byteLength;
+
+//     for (let i = 0; i < len; i++) {
+//         binary.push(String.fromCharCode(bytes[i]));
+//     }
+
+//     return btoa(binary.join(''));
+// }
